b7479e94cd6dafade1eb30613171c8086009a1cc
boinq <boinq@boinq-Pc>
2013-02-17 05:46:36 +0100

    Init

---
+++ Eve Manufacturing/src/dk/boinq/evethingy/application/Main.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ */
+package dk.boinq.evethingy.application;
+
+/**
+ *
+ * @author boinq
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
